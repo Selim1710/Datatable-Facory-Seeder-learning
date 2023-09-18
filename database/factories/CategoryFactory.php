8aa2fbf2bb1a433fb2e338510f7d0398e2d82ada
Md. Selim Hossain Suhag <93088169+Selim1710@users.noreply.github.com>
2023-09-18 12:29:48 +0600

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,6 +17,15 @@ class CategoryFactory extends Factory
             'name'=> $this->faker->name(),
             'image'=>$this->faker->image('public/backend/images',640,480, null, false),
             'description'=>$this->faker->sentence(),
+            
+            /**
+            'date'=>$this->faker->date('Y-m-d'),
+            'time'=>$this->faker->time('H:i:s'),
+            'role'=>'guest',
+            
+            */
+            
+            
         ];
     }
 }
